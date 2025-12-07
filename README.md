000bf8b4160cb8d7b9451edb2e3f2f0fdd94e975
Eddie Aljamal <aljamaleddie@gmail.com>
2025-12-07 14:04:23 -0500

    clean up branch

--- README.md
+++
@@ -1,5 +0,0 @@
-# Diff-SINDy
-
-<p align="center">
-  <img src="logo.png" width="300" title="logo">
-</p>
--- src/architectures/__init__.py
+++
@@ -1,4 +0,0 @@
-from .FourierFeatures import FourierFeatures
-from .MLP import MLP
-from .MultipleMLPs import MultipleMLPs
-from .MultiTaskMLP import MultiTaskMLP
