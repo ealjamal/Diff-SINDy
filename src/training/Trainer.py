7f4bd49d6388346db90d07051738175687137b86
Eddie Aljamal <aljamaleddie@gmail.com>
2025-12-10 10:20:24 -0500

    Add documentation

--- src/training/Trainer.py
+++ src/training/Trainer.py
@@ -3,6 +3,12 @@ from utils.derivatives import compute_derivatives
 import torch.nn.functional as F
 
 class Trainer:
+    '''
+    A class to implement the two stage training of Diff-SINDy PDE discovery
+    with optional warmup and gradient balancing.
+    
+    '''
+
     def __init__(self, 
                  lhs_deriv,
                  deriv_library,
@@ -27,6 +33,114 @@ class Trainer:
                  lambda_pde=1.0,
                  lambda_alpha=0.9,
                  verbosity=0):
+        
+        '''
+        --------
+        Arguments
+        --------
+        
+            lhs_derive: str
+                A string denoting the LHS derivative. For example, 'u_t' denotes
+                the first time derivative of u_pred and 'u_tt' the second time derivative.
+                AD will be used to compute this derivative.
+                
+            deriv_library: list of str
+                The list of derivatives that are possibly in the PDE. AD will be used to
+                compute these derivatives.
+                
+            add_const_coeff: bool
+                If True, this will concatenate a columns of 1s in the 0th index
+                to account for the forcing term in the PDE.
+                
+            input_names: list of str
+                The input names. For example ['x', 't'] denotes 1D space and time.
+                These must align with the dimensions of the input data.
+
+            coeffs_input_names: list of str
+                The input names of the variables that the coefficient will be a
+                a function of. These are the inputs to the coefficients network(s).
+                For example, ['x', 't'] or ['x', 't', 'u'], the latter includes
+                the approximated solution.
+
+            sol_optimizer: torch.optim
+                The optimizer for the solution network.
+
+            coeffs_optimizer: torch.optim
+                The optimizer for the coefficients network.
+
+            sol_lr_scheduler:
+                Learning rate scheduler for the solution warm up only monitoring
+                the warm up loss of IC, BC and interior data. Default is None, 
+                meaning no learning rate scheduler.
+
+            fit_lr_scheduler:
+                Learning rate scheduler for the two-stage training procedure
+                of the solution and coefficients network(s). This monitors the
+                total loss of IC, BC, interior data, and PDE losses. Default is 
+                None, meaning no learning rate scheduler.
+
+            n_epochs: int
+                Number of epochs to train. Default is 2000.
+
+            sol_warmup_n_epochs: int
+                Number of epochs to warm up the solution network (without PDE
+                loss). Default is 0, meaning no warm up.
+
+            coeffs_update_per_epoch: int
+                The number of update steps for the solutions and coefficients 
+                network(s) in each epoch. For each epoch in n_epochs, this is 
+                how many times stage 1 (solution training) and stage 2 
+                (coefficients training) will be optimized separately. Defualt
+                is 10.
+
+            lambda_pde_warmup_epochs: int
+                The number of epochs for which lambda_pde will gradually increase
+                to its maximum value. For subsequent epochs, the value of lambda_pde
+                will be constant and equal to its max_lambda_pde value. Default
+                is 1000, half of the default value for n_epochs.
+
+            max_lambda_pde: float
+                The maximum weighting on the PDE loss. Default is 1.
+
+            sol_warmup_loss_weighting: bool
+                If True, gradient balancing will be implemented while
+                warming the solution network of the IC and BC losses
+                wrt. the interior data loss. Default is True.
+
+            fit_loss_weighting: bool
+                If True, gradient balancing will be implemented while
+                performing the two-stage training procedure of the IC, BC,
+                and interior data losses wrt. the PDE loss. Default is True.
+
+            lambda_ic: float
+                The weight on the IC loss. This will be updated if gradient
+                balancing is implemented for both the warmup and the two-stage
+                training. Default is 1.
+
+            lambda_bc: float
+                The weight on the BC loss. This will be updated if gradient
+                balancing is implemented for both the warmup and the two-stage
+                training. . Default is 1.
+
+            lambda_data: float
+                The weight on the interior data loss. This will be updated if 
+                gradient balancing is implemented for only the two-stage training.
+                Default is 1.
+
+            lambda_pde:
+                The weight on the PDE loss. Default is 1.
+
+            lambda_alpha: float
+                The weight of the update for lambda_ic and lambda_bc. The weight of the 
+                previous value will be (1 - lambda_alpha). Therefore, lambda_alpha
+                must be between 0 and 1. Default is 0.9.
+
+            verbosity:
+                If equal to 0. No messages tracking the losses will be printed.
+                If not equal to 0, this is the incremental number of epochs in 
+                n_epochs when messages for the losses will be printed.   
+            
+        '''
         
         self.lhs_deriv = lhs_deriv
         self.deriv_library = deriv_library
@@ -69,14 +183,23 @@ class Trainer:
         self.X_col = X_col
 
     def _compute_warmup_losses(self, lambda_ic, lambda_bc, lambda_data):
+        '''
+        Compute the IC, BC, and interior data losses for the warmup.
+        
+        '''
+
+        # Compute the approximated solution using the solution model.
         u_pred_ic = self.sol_model(self.X_ic)
         u_pred_bc = self.sol_model(self.X_bc)
         u_pred_data = self.sol_model(self.X_data)
 
+        # Compute IC, BC, and interior data losses.
         loss_ic = F.mse_loss(u_pred_ic, self.U_ic)
         loss_bc = F.mse_loss(u_pred_bc, self.U_bc)
         loss_data = F.mse_loss(u_pred_data, self.U_data)
         
+        # Implement gradient balancing if needed for IC and BC losses
+        # with respect to interior data loss.
         if self.sol_warmup_loss_weighting:
             lambda_ic, lambda_bc = loss_weighting_data_wang(sol_model=self.sol_model,
                                                               loss_ic=loss_ic,
@@ -85,12 +208,18 @@ class Trainer:
                                                               lambda_ic=lambda_ic,
                                                               lambda_bc=lambda_bc,
                                                               lambda_alpha=self.lambda_alpha)
-
+        
+        # Calculate the total loss using IC, BC, and interior data losses.
         loss_total = lambda_ic * loss_ic + lambda_bc * loss_bc + lambda_data * loss_data
 
         return loss_total, loss_ic, loss_bc, loss_data, lambda_ic, lambda_bc, lambda_data
     
     def define_coeffs_input(self, X, U):
+        '''
+        Helper function to get the input data for the coefficients network(s).
+        
+        '''
+
         if 'u' in self.coeffs_input_names:
             coeffs_input = torch.cat([X[:, self.coeffs_input_idxs], U], dim=-1)
         else:
@@ -99,17 +228,28 @@ class Trainer:
         return coeffs_input
 
     def _compute_losses(self, lambda_ic, lambda_bc, lambda_data, lambda_pde):
+        '''
+        Compute the losses for the two-stage training procedure using
+        IC, BC, interior data, and collocation points.
+        
+        '''
+        
+        # Turn on rquires_grad so we can take derivatives for the derivative library.
         self.X_col.requires_grad_(True)
 
+        # Compute the approximated solution using the solution model.
         u_pred_ic = self.sol_model(self.X_ic)
         u_pred_bc = self.sol_model(self.X_bc)
         u_pred_data = self.sol_model(self.X_data)
         u_pred_col = self.sol_model(self.X_col)
 
+        # Compute IC, BC, and interior data losses.
         loss_ic = F.mse_loss(u_pred_ic, self.U_ic)
         loss_bc = F.mse_loss(u_pred_bc, self.U_bc)
         loss_data = F.mse_loss(u_pred_data, self.U_data)
 
+        # Calculate the LHS derivative and the derivatives in the derivative 
+        # library.
         lhs_d, D = compute_derivatives(u_pred=u_pred_col,
                                        inputs=self.X_col,
                                        lhs_deriv=self.lhs_deriv,
@@ -117,12 +257,17 @@ class Trainer:
                                        input_names=self.input_names,
                                        add_ones=self.add_const_coeff)
         
+        # Define the inputs to the coefficient network(s).
         coeffs_input = self.define_coeffs_input(self.X_col, u_pred_col)
+        # Apply the coefficients network(s) on the inputs.
         coeffs_pred = self.coeffs_model(coeffs_input)
+        # Compute the RHS of the PDE.
         pde = torch.sum(D * coeffs_pred, dim=-1, keepdims=True)
-
+        # Compute the PDE loss.
         loss_pde = F.mse_loss(pde, lhs_d)
-
+        
+        # Implement gradient balancing if needed for IC, BC, and interior data
+        # losses with respect to PDE loss.
         if self.fit_loss_weighting:
             lambda_ic, lambda_bc, lambda_data = loss_weighting_pde_wang(sol_model=self.sol_model,
                                                                           loss_ic=loss_ic,
@@ -134,47 +279,75 @@ class Trainer:
                                                                           lambda_data=lambda_data,
                                                                           lambda_alpha=self.lambda_alpha)
 
+        # Calculate the total loss using IC, BC, interior data, and PDE losses.
         loss_total = lambda_ic * loss_ic + lambda_bc * loss_bc + lambda_data * loss_data + lambda_pde * loss_pde
 
         return loss_total, loss_ic, loss_bc, loss_data, loss_pde, lambda_ic, lambda_bc, lambda_data, lambda_pde
     
     def sol_warmup_step(self, lambda_ic, lambda_bc):
+        '''
+        Implements an optimization step for the solution warmup.
+
+        '''
+
         self.sol_optimizer.zero_grad()
+        # Compute IC, BC, and interior data losses.
         loss_total, loss_ic, loss_bc, loss_data, lambda_ic, lambda_bc, lambda_data = self._compute_warmup_losses(lambda_ic, lambda_bc, self.lambda_data)
         
         loss_total.backward()
         self.sol_optimizer.step()
+        # Update learning rate using scheduler if needed.
         if self.sol_lr_scheduler is not None:
             self.sol_lr_scheduler.step(loss_total.item())
             
         return loss_total.item(), loss_ic.item(), loss_bc.item(), loss_data.item(), lambda_ic, lambda_bc, lambda_data
     
     def sol_step(self, lambda_ic, lambda_bc, lambda_data, lambda_pde):
+        '''
+        Implements an optimization step for the stage 1 of the two-stage
+        training procedure in which the solution network is trained using
+        the IC, BC, interior data and PDE losses with the coefficient newtork
+        frozen.
+
+        '''
+
         self.sol_optimizer.zero_grad()
+        # Compute IC, BC, interior data, and PDE losses.
         (loss_total, loss_ic, loss_bc, loss_data, loss_pde, 
          lambda_ic, lambda_bc, lambda_data, lambda_pde) = self._compute_losses(lambda_ic, lambda_bc, lambda_data, lambda_pde)
         
         loss_total.backward()
         self.sol_optimizer.step()
+        # Update learning rate using scheduler if needed.
         if self.fit_lr_scheduler is not None:
             self.fit_lr_scheduler.step(loss_total.item())
 
         return loss_total.item(), loss_ic.item(), loss_bc.item(), loss_data.item(), loss_pde.item(), lambda_ic, lambda_bc, lambda_data, lambda_pde
     
     def coeffs_step(self):
+        '''
+        Implements an optimization step for the stage 12of the two-stage
+        training procedure in which the coefficient network is trained using
+        only the PDE loss with the solution network newtork frozen.
+
+        '''
 
         self.coeffs_optimizer.zero_grad()
         self.X_col.requires_grad_(True)
+        # Compute the approximate solution at the collocation points.
         u_pred_col = self.sol_model(self.X_col)
 
+        # Calculate LHS and derivative library derivatives.
         lhs_d, D = compute_derivatives(u_pred=u_pred_col, inputs=self.X_col, lhs_deriv=self.lhs_deriv,
                                        deriv_library=self.deriv_library, input_names=self.input_names,
                                        add_ones=self.add_const_coeff)
-        
+        # Define the inputs for the coefficients network(s).
         coeffs_input = self.define_coeffs_input(self.X_col, u_pred_col)
+        # Compute the coefficients.
         coeffs_pred = self.coeffs_model(coeffs_input)
+        # Compute the RHS of the PDE.
         pde = torch.sum(D * coeffs_pred, dim=-1, keepdims=True)
-
+        # Calculate PDE loss.
         loss = F.mse_loss(pde, lhs_d)
         loss.backward()
         self.coeffs_optimizer.step()
@@ -182,27 +355,76 @@ class Trainer:
         return loss.item()
     
     def fit(self, sol_model, coeffs_model, X_ic, U_ic, X_bc, U_bc, X_data, U_data, X_col):
+        '''
+        Fit the solution and coefficients network(s) using IC, BC, interior data,
+        and collocation points to discover a PDE using a two-stage procedure. Stage
+        1 will train the solution network using the IC, BC, interior data losses with
+        the PDE loss being added gradually while the coefficient network is frozen.
+        In Stage 2, the solution network will be frozen and the coefficients 
+        network(s) will be training using the PDE loss.
+
+        --------
+        Arguments
+        --------
+
+            sol_model:
+                The solution model.
+            
+            coeffs_model:
+                The coefficients model.
+
+            X_ic: torch.Tensor
+                The spatio-temporal input IC data.
+            
+            U_ic: torch.Tensor
+                The observed solution at the IC points.
+
+            X_bc: torch.Tensor
+                The spatio-temporal input BC data.
+            
+            U_bc: torch.Tensor
+                The observed solution at the BC points.
+            
+            X_data: torch.Tensor
+                The spatio-temporal input interior data.
+            
+            U_data: torch.Tensor
+                The observed solution at the interior points.
+
+            X_data: torch.Tensor
+                The spatio-temporal input collocation data.
+
+        '''
+
+        # Prepare models and data.
         self.prepare_sol_model(sol_model=sol_model)
         self.prepare_coeffs_model(coeffs_model=coeffs_model)
         self.prepare_data(X_ic=X_ic, U_ic=U_ic, X_bc=X_bc, U_bc=U_bc, X_data=X_data, U_data=U_data, X_col=X_col)
 
+        # Define lists to track losses.
         total_losses = []
         ic_losses = []
         bc_losses = []
         data_losses = []
         pde_losses = []
 
+        # Warmup training of the solution.
         lambda_ic, lambda_bc, lambda_data = self.lambda_ic, self.lambda_bc, self.lambda_data
         self.sol_model.train()
         for warmup_epoch in range(self.sol_warmup_n_epochs):
             loss_total, loss_ic, loss_bc, loss_data, lambda_ic, lambda_bc, lambda_data = self.sol_warmup_step(lambda_ic, lambda_bc)
 
+            # Track losses during warmup.
             if self.verbosity > 0 and (warmup_epoch + 1) % self.verbosity == 0:
                 print(f"[SolWarmup] Ep {warmup_epoch + 1}/{self.sol_warmup_n_epochs} Total={loss_total:.6e}, "
                         f"IC={loss_ic:.6e}, BC={loss_bc:.6e}, Data={loss_data:.6e}, "
                         f"L_IC={lambda_ic:.6e}, L_BC={lambda_bc:.6e}, L_data={lambda_data:.6e}")
 
+        # Two stage training procedure.
         for epoch in range(self.n_epochs):
+            # Stage 1: freeze coefficients model and train solution model
+            # using IC, BC, interior data, and PDE losses with the last
+            # being gradually added with lambda_pde scheduling.
             for param in self.sol_model.parameters():
                 param.requires_grad_(True)
             for param in self.coeffs_model.parameters():
@@ -216,6 +438,8 @@ class Trainer:
                 (loss_total, loss_ic, loss_bc, loss_data, loss_pde, 
                 lambda_ic, lambda_bc, lambda_data, lambda_pde) = self.sol_step(lambda_ic, lambda_bc, lambda_data, lambda_pde)
 
+            # Stage 2: freeze solution model and train coefficient model using
+            # PDe loss.
             for param in self.sol_model.parameters():
                 param.requires_grad_(False)
             for param in self.coeffs_model.parameters():
@@ -232,11 +456,13 @@ class Trainer:
                 data_losses.append(loss_data)
                 pde_losses.append(loss_pde)
 
+            # Track losses.
             if self.verbosity > 0 and (epoch + 1) % self.verbosity == 0:
                 print(f"[Train] Ep {epoch + 1}/{self.n_epochs} Total={loss_total:.6e}, "
                         f"IC={loss_ic:.6e}, BC={loss_bc:.6e}, Data={loss_data:.6e}, PDE={loss_pde:.6e}, "
                         f"L_IC={lambda_ic:.6e}, L_BC={lambda_bc:.6e}, L_data={lambda_data:.6e}, L_pde={lambda_pde:.6e}")
 
+        # Save losses and return them.
         res = dict()
         res['total_losses'] = total_losses
         res['ic_losses'] = ic_losses
--- src/utils/loss_weighting.py
+++ src/utils/loss_weighting.py
@@ -1,61 +1,205 @@
 import torch
 
+# The following functions will implement loss weighting schemes either with 
+# or without the PDE loss included. We follow the treatment in 
+# Wang et al. (2021) https://arxiv.org/abs/2001.04536.
+
 def grad_norm(loss, 
               params, 
               eps=1e-8):
+  '''
+  Compute the L2 norm of the loss gradients wrt. the parameters.
+
+  --------
+  Arguments
+  --------
+    
+    loss: float
+      The loss for which we calculate the L2 norm of the gradient wrt. 
+      the parameters.
+
+    params:
+      The parameters or weights that lead to the loss
+
+    eps: float
+      A small addition to the norm to prevent it from being zero and leading
+      to divergences. Default is 1e-8.
+
+    --------
+    Output
+    --------
+
+    norm_grad: float
+      The L2 norm of the loss gradient wrt. its parameters.
+
+  '''
   
+  # Collect the parameters that are learnable.
   params = [p for p in params if p.requires_grad]
+  # If there are no parameters that are learnable we return 1.
   if len(params) == 0:
     return torch.tensor(1.0)
+  # Take the gradient of the loss wrt. parameters.
   grads = torch.autograd.grad(loss, params,
                               retain_graph=True, create_graph=False)
+  # Collect gradients with respect to parameters.
   grads = [g for g in grads if g is not None]
 
-  return torch.sqrt(sum(g.pow(2).sum() for g in grads)) + eps
+  # Calculate the L2 norm of gradients.
+  norm_grad = torch.sqrt(sum(g.pow(2).sum() for g in grads)) + eps
+
+  return norm_grad
 
 def loss_weighting_data_wang(sol_model,
-                               loss_ic,
-                               loss_bc,
-                               loss_data,
-                               lambda_ic,
-                               lambda_bc,
-                               lambda_alpha):
+                             loss_ic,
+                             loss_bc,
+                             loss_data,
+                             lambda_ic,
+                             lambda_bc,
+                             lambda_alpha):
+    '''
+    Perform moving average weighting on the initial condition (IC)
+    and boundary condition (BC) losses with respect to the interior data loss.
 
+    --------
+    Arguments
+    --------
+
+      sol_model:
+        The solution model given by some MLP architecture.
+
+      loss_ic: 
+        The loss of initial condition (IC).
+
+      loss_bc:
+        The loss of boundary conditions (BCs).
+
+      loss_data:
+        The loss of the interior data.
+
+      lambda_ic: float
+        The weight on the IC loss. This will be updated.
+
+      lambda_bc: float
+        The weight on the BC loss. This will be updated.
+
+      lambda_alpha: float
+        The weight of the update for lambda_ic and lambda_bc. The weight of the 
+        previous value will be (1 - lambda_alpha). Therefore, lambda_alpha
+        must be between 0 and 1.
+
+    --------
+    Output
+    --------
+    
+      Return a tuple of updated lambda_ic and lambda_bc, using a moving average.
+        
+      lambda_ic: float
+        The updated weight for the IC loss.
+
+      lambda_bc: float
+        The updated weight for the BC loss.
+
+    '''
+
+    # Calculate gradients for IC, BC, and interior data losses.
     g_ic = grad_norm(loss_ic, sol_model.parameters())
     g_bc = grad_norm(loss_bc, sol_model.parameters())
     g_data = grad_norm(loss_data, sol_model.parameters())
 
+    # Take the max of the interior data loss gradient.
     g_ref = torch.max(g_data.detach())
 
+    # Calculate the new weighting for IC and BC losses.
     lambda_ic_new = g_ref/g_ic.detach()
     lambda_bc_new = g_ref/g_bc.detach()
 
+    # Perform moving average of the weights on IC and BC losses.
     lambda_ic = (1 - lambda_alpha) * lambda_ic + lambda_alpha * lambda_ic_new
     lambda_bc = (1 - lambda_alpha) * lambda_bc + lambda_alpha * lambda_bc_new
 
     return lambda_ic.detach(), lambda_bc.detach()
 
 def loss_weighting_pde_wang(sol_model,
-                              loss_ic,
-                              loss_bc,
-                              loss_data,
-                              loss_pde,
-                              lambda_ic,
-                              lambda_bc,
-                              lambda_data,
-                              lambda_alpha):
+                            loss_ic,
+                            loss_bc,
+                            loss_data,
+                            loss_pde,
+                            lambda_ic,
+                            lambda_bc,
+                            lambda_data,
+                            lambda_alpha):
+    '''
+    Perform moving average weighting on the initial condition (IC), 
+    boundary condition (BC), and interior data losses with respect to 
+    the PDE loss.
 
+    --------
+    Arguments
+    --------
+
+      sol_model:
+        The solution model given by some MLP architecture.
+
+      loss_ic: 
+        The loss of initial condition (IC).
+
+      loss_bc:
+        The loss of boundary conditions (BCs).
+
+      loss_data:
+        The loss of the interior data.
+
+      loss_pde:
+        The loss of the PDE.
+
+      lambda_ic: float
+        The weight on the IC loss. This will be updated.
+
+      lambda_bc: float
+        The weight on the BC loss. This will be updated.
+
+      lambda_data: float
+        The weight on the interior data loss. This will be updated.
+
+      lambda_alpha: float
+        The weight of the update for lambda_ic and lambda_bc. The weight of the 
+        previous value will be (1 - lambda_alpha). Therefore, lambda_alpha
+        must be between 0 and 1.
+
+    --------
+    Output
+    --------
+    
+      Return a tuple of updated lambda_ic, lambda_bc, lambda_data 
+      using a moving average.
+        
+      lambda_ic: float
+        The updated weight for the IC loss.
+
+      lambda_bc: float
+        The updated weight for the BC loss.
+
+      lambda_data: float
+        The updated weight for the interior data loss.
+
+    '''
+
+    # Calculate gradients for IC, BC, interior data, and PDE losses.
     g_ic = grad_norm(loss_ic, sol_model.parameters())
     g_bc = grad_norm(loss_bc, sol_model.parameters())
     g_data = grad_norm(loss_data, sol_model.parameters())
     g_pde = grad_norm(loss_pde, sol_model.parameters())
 
+    # Take the max of the PDE loss gradient.
     g_ref = torch.max(g_pde.detach())
 
+    # Calculate the new weighting for IC, BC, and interior data losses.
     lambda_ic_new = g_ref/g_ic.detach()
     lambda_bc_new = g_ref/g_bc.detach()
     lambda_data_new = g_ref/g_data.detach()
 
+    # Perform moving average of the weights on IC, BC, and interior data losses.
     lambda_ic = (1 - lambda_alpha) * lambda_ic + lambda_alpha * lambda_ic_new
     lambda_bc = (1 - lambda_alpha) * lambda_bc + lambda_alpha * lambda_bc_new
     lambda_data = (1 - lambda_alpha) * lambda_data + lambda_alpha * lambda_data_new
@@ -63,6 +207,24 @@ def loss_weighting_pde_wang(sol_model,
     return lambda_ic.detach(), lambda_bc.detach(), lambda_data.detach()
 
 def lambda_schedule(step, warmup_steps, max_lambda):
+    '''
+    A scheduler to gradually increase the weighting of the loss.
+
+    --------
+    Arguments
+    --------
+      step: int
+        The current epoch.
+
+      warm_steps: int
+        The number of epochs for which to gradually increase the loss weight.
+
+      max_lambda: float
+        The maximum weighting that will be achieved. After warm_steps epochs,
+        this will be the value of the weighting for all subsequent epochs.
+    
+    '''
+
     if step < warmup_steps:
         return max_lambda * (step / warmup_steps)
     
